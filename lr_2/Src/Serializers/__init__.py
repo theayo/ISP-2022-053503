597fb42e43015d7f7ef2503e11f39dfe083d77c1
theayo <kekover1337@gmail.com>
2022-05-17 23:36:22 +0300

    fill

--- lr_2/Src/Serializers/__init__.py
+++ lr_2/Src/Serializers/__init__.py
@@ -1,2 +1,5 @@
 from Src.Serializers.yaml_parser import YamlSerializer
+from Src.Serializers.json_parser import JsonSerializer
+from Src.Serializers.toml_parser import TomlSerializer
+from Src.Serializers.ser_factory import SerializerFactory
 
